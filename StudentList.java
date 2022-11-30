e8141279cd3676d950e9cf302eb8c89c8acab2e5
Jahangir Alam Rocky <x00jahangir@gmail.com>
2022-11-30 14:35:58 +0600

    step#4

--- StudentList.java
+++ StudentList.java
@@ -3,15 +3,33 @@ import java.text.*;
 import java.util.*;
 
 public class StudentList {
+    public static BufferedReader getBufferedReader(){
+        try{
+            BufferedReader bufferedReader = new BufferedReader(
+                    new InputStreamReader(
+                            new FileInputStream("students.txt")));
+        }catch (FileNotFoundException e){
+            e.printStackTrace();
+        }
+        return null;
+    }
+    public static BufferedWriter getBufferedWriter(){
+        try {
+            BufferedWriter bufferedWriter = new BufferedWriter(
+                    new FileWriter("students.txt", true));
+            return  bufferedWriter;
+        }catch (Exception e){
+            System.out.println(e);
+        }
+        return  null;
+    }
     public static void main(String[] args) {
 
 //		Check arguments
         if (args[0].equals("a")) {
             System.out.println("Loading data ...");
             try {
-                BufferedReader bufferedReader = new BufferedReader(
-                        new InputStreamReader(
-                                new FileInputStream("students.txt")));
+                BufferedReader bufferedReader = getBufferedReader();
                 String line = bufferedReader.readLine();
                 String studentNames[] = line.split(", ");
                 for (String studentName : studentNames) {
@@ -23,9 +41,7 @@ public class StudentList {
         } else if (args[0].equals("r")) {
             System.out.println("Loading data ...");
             try {
-                BufferedReader bufferedReader = new BufferedReader(
-                        new InputStreamReader(
-                                new FileInputStream("students.txt")));
+               BufferedReader bufferedReader = getBufferedReader();
                 String line = bufferedReader.readLine();
                 System.out.println(line);
                 String studentNames[] = line.split(",");
@@ -38,8 +54,7 @@ public class StudentList {
         } else if (args[0].contains("+")) {
             System.out.println("Loading data ...");
             try {
-                BufferedWriter bufferedWriter = new BufferedWriter(
-                        new FileWriter("students.txt", true));
+               BufferedWriter bufferedWriter = getBufferedWriter();
                 String newStudentName = args[0].substring(1);
                 Date date = new Date();
                 String dateformate = "dd/MM/yyyy-hh:mm:ss a";
@@ -54,9 +69,7 @@ public class StudentList {
         } else if (args[0].contains("?")) {
             System.out.println("Loading data ...");
             try {
-                BufferedReader bufferedReader = new BufferedReader(
-                        new InputStreamReader(
-                                new FileInputStream("students.txt")));
+                BufferedReader bufferedReader = getBufferedReader();
                 String line = bufferedReader.readLine();
                 if (line.contains(args[0].substring(1))) {
                     System.out.println("We Found it");
@@ -70,9 +83,7 @@ public class StudentList {
         } else if (args[0].contains("c")) {
             System.out.println("Loading data ...");
             try {
-                BufferedReader bufferedReader = new BufferedReader(
-                        new InputStreamReader(
-                                new FileInputStream("students.txt")));
+                BufferedReader bufferedReader = getBufferedReader();
                 String line = bufferedReader.readLine();
                 String names[] = line.split(",");
                 System.out.println(names.length / 2 + " words found");
