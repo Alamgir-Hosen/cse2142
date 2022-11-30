b08c620232d4780e88e99f470d26700e31553d22
Jahangir Alam Rocky <x00jahangir@gmail.com>
2022-11-30 15:10:22 +0600

    Step#5

---
+++ Constants.java
@@ -0,0 +1,9 @@
+public class Constants {
+    final String StudentList = "students.txt";
+    final String ShowAll = "a";
+    final String ShowRandom = "r";
+    final String ShowCount = "c";
+    final String FindEntry = "?";
+    final String AddEntry = "+";
+    final String StudentEntryDelimiter = ",";
+}
